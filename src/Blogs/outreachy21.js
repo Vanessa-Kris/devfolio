d7b527b7406d60bba468b5a1ebe2ae4570e42003
Minion <nessakristo@gmail.com>
2023-10-06 15:23:09 +0100

    reduced blog size

--- src/Blogs/outreachy21.js
+++ src/Blogs/outreachy21.js
@@ -3,23 +3,23 @@ import React from "react";
 
 export default function outreachy21() {
   return (
-    <Box>
-      <Typography sx={{ py: 3 }}>
+    <Box sx={{ color: "#fff" }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         Hey There! I'd like to share my experience from my Outreachy internship
         in 2021/2022.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         During this period, I worked as a package manager/maintainer with
         Fedora, and my mentors were Ankur Sinha and Ben Cotton.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         Outreachy is a three-month paid internship program designed to support
         underrepresented individuals in the tech industry.{" "}
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         I must say that Outreachy has been one of the most remarkable
         experiences of my life, without a doubt. In August 2021, I submitted my
         initial application, where I highlighted my underrepresented status in
@@ -28,7 +28,7 @@ export default function outreachy21() {
         as required.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         As a newcomer, I had serious doubts about my abilities and didn't set my
         hopes too high. However, in October, when the results of the initial
         applications were released, I received the acceptance email! That
@@ -36,20 +36,20 @@ export default function outreachy21() {
         let it slip through my fingers.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ my: 3 }}>
         On the same night I received my acceptance email, I began researching
         projects to contribute to. It's worth noting that at that time, I had
         limited technical skills, aside from basic laptop operation and some
         knowledge of HTML.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ my: 3 }}>
         The Fedora project particularly piqued my interest because they were
         willing to teach newcomers, and the project descriptions were both
         intriguing and challenging, which I love.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ my: 3 }}>
         I joined the Fedora Matrix channel, introduced myself as required, and
         had the opportunity to meet my mentors and fellow interns. My incredible
         mentors provided all the necessary resources to get me started. I began
@@ -59,7 +59,7 @@ export default function outreachy21() {
         roadblock, I could always count on my mentors for assistance.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ my: 3 }}>
         I made sure to document all my contributions and provide support to my
         fellow interns because Outreachy is all about fostering a sense of
         community. After the contribution period, I submitted my final
@@ -67,7 +67,7 @@ export default function outreachy21() {
         attending meetings and actively participating as a team member.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ my: 3 }}>
         When my final application was approved, I felt unstoppable (laughs). It
         was truly one of the best moments of my life. In week 1, I had to
         complete all my pending tasks, and then came the challenge of blogging
@@ -76,14 +76,14 @@ export default function outreachy21() {
         of what I know now; he's an incredible mentor.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ my: 3 }}>
         Despite my subpar blogging skills, I persisted and continued blogging
         until the final week (laughs). I am immensely grateful that I embarked
         on the Outreachy journey. It provided me with the confidence and support
         I needed to kickstart my career.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ my: 3 }}>
         Curious about what I've been up to since then?
       </Typography>
     </Box>
--- src/Blogs/transition.js
+++ src/Blogs/transition.js
@@ -3,15 +3,15 @@ import React from "react";
 
 export default function Transition() {
   return (
-    <Box>
-      <Typography sx={{ py: 3 }}>
+    <Box sx={{ color: "#fff" }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         In December 2019, I had just relocated to Cameroon, leaving my school
         and everything behind in an attempt to start anew. At that time, I had
         absolutely no idea about my passions or what I wanted to pursue. I found
         myself amidst a mid-life crisis and numerous uncertainties.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         For nearly two years, I remained idle at home until 2021. It was during
         this period that I had a very close friend who happened to be a
         developer (and still is). I became increasingly intrigued and curious
@@ -20,21 +20,21 @@ export default function Transition() {
         with HTML.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         Around July 2021, I came across Outreachy, which offered a paid remote
         internship opportunity. I saw it as the perfect chance to get myself
         into the tech industry. Although I was scared, I was resolute in my
         determination to pursue this path.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         Transitioning into the tech field had its own set of challenges,
         including access to learning resources, consistent electricity, and
         covering internet bills. However, I firmly believed that there is always
         a solution or workaround to every problem.
       </Typography>
       {/*  */}
-      <Typography sx={{ py: 3 }}>
+      <Typography variant="h6" sx={{ py: 3 }}>
         For more details about my Outreachy experience, please check out my
         other post.
       </Typography>
